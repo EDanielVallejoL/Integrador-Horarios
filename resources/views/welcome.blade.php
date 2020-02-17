0697ad38efe277da661d47a1c811cdadebeadae2
JonathanBaroL <jonathan.developer21@gmail.com>
2020-02-17 17:06:22 -0600

    prueba1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    El Baro Rules
                 </div>
 
                 <div class="links">
