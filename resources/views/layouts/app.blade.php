3f38804ab09606471a428c0d7db4df35dd1787cf
Daniel Vallejo <edaniel.vallejol@outlook.com>
2020-02-24 11:35:51 -0600

    no encuentro la de reset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Petbook</title>
+    <title>Sistema de Horarios Inteligente</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
