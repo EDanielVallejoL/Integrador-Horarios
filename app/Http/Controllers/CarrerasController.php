f44712964eac3ae1bc37790677dd133e7f5e078a
Daniel Vallejo <edaniel.vallejol@outlook.com>
2020-03-13 02:14:46 -0600

    me rife

--- app/Http/Controllers/CarrerasController.php
+++ app/Http/Controllers/CarrerasController.php
@@ -33,6 +33,42 @@ Class PlantillaXCarrera
     }
 }
 
+class Materias
+{
+    public $Materia;
+    public $valorGrupo;
+    public $valorMateria;
+
+
+    public function __construct($Materia,$valorGrupo,$valorMateria)
+    {
+        $this->Materia = $Materia;
+        $this->valorGrupo = $valorGrupo;
+        $this->valorMateria = $valorMateria;
+        
+    }  
+}
+
+class Carrera
+{
+    //nombre de la carrera
+    public $nombreCarrera;
+    //lista de objeto carrera
+    public $listaMaterias = array();
+    //promedio total de la carrera
+    public $PromedioCarrera;
+
+
+    public function __construct($nombreCarrera, array $listaMaterias = [], $PromedioCarrera)
+    {
+        $this->nombreCarrera = $nombreCarrera;
+        $this->listaMaterias = $listaMaterias;
+        $this->PromedioCarrera = $PromedioCarrera;
+        
+    }
+}
+
+
 
 class MateriaXCarrera
 {
@@ -220,8 +256,24 @@ class CarrerasController extends Controller
             //Creacion de las materias por carreras regresa {}
             $listaMateriasxCarrera = $this->leeMPC($name);
             $listaGrupos = $this->leeHorariosCompletos($name2);
-            $listaABS = $this->ObtenPlantilla($listaMateriasxCarrera,$listaGrupos);
-            $listashoras = $this->llenaPlantillas($listaABS);
+           // $listaABS = $this->ObtenPlantilla($listaMateriasxCarrera,$listaGrupos);
+           // $listashoras = $this->llenaPlantillas($listaABS);
+            $listaFinal = $this->Carreras($name);
+            $this->AsignaValor($listaFinal,$listaGrupos);
+
+            foreach($listaFinal as $lf)
+            {
+                echo '<h2>'.$lf->nombreCarrera.'</h2>';
+                foreach($lf->listaMaterias as $listass){
+                        echo $listass->Materia.'       '; 
+                        echo 'Valor por Grupo: '.$listass->valorGrupo.'       ';
+                        echo 'Valor por Materia: '.$listass->valorMateria.'       ';     
+                        echo '<br>';
+                }
+                echo'<br>';
+                echo 'El promedio de la carrera es: '.$lf->PromedioCarrera;
+                
+            }
         } else {
             return "Fallo al cargar archivo, intenta de nuevo";
         }
@@ -352,6 +404,9 @@ class CarrerasController extends Controller
                 }
             }
         }
+
+
+
         return $listaMateriasxCarrera;
     }
 
@@ -530,6 +585,7 @@ class CarrerasController extends Controller
         return $listaABS;  
     }
 
+    //Miguel
     public function llenaPlantillas($listaAbs)
     {
         //esta bandera nos indica el cambio de carrera
@@ -537,11 +593,7 @@ class CarrerasController extends Controller
         $bandera = 0;
         foreach($listaAbs as $a)
             {
-                if($bandera == 0)
-                {
                     //es la primera vez que entra
-                    $temp = $a->Carrera;
-                    $bandera = 1;
                     echo'<h2>'.'Carrera: '.$a->Carrera.'</h2>';
                     echo $Lun = $a->lunes;
                     echo '   ';
@@ -554,20 +606,160 @@ class CarrerasController extends Controller
                     $sab = $a->lunes;*/
                     //$rest = substr($Lun,0, -5);
                     echo '<br>';
-                }else{
-                    
-                    if($temp == $a->Carrera)
-                    {
-                        $bandera = 0;
-                        echo $Lun = $a->lunes;
-                        echo '   ';
-                        echo $Materia = $a->nombreMateria;
-                        echo '<br>';
-                    }else{
-
-                    }
-                }                    
+                                
             }
             return $listahoras;
     }
+
+
+
+
+    //aqui de dos listas que teniamos ahora se convirtio en una sola
+    //ahora en una sola lista de objetos se puede acceder a las carreras con sus propiedades
+    //y a las materias por carrera con sus propiedades
+    public function Carreras($nombreArchivo)
+    {
+        $rutaArchivo = \public_path() . '/archivos/' . $nombreArchivo;
+        $documento = IOFactory::load($rutaArchivo);
+
+        # obtenemos la primera celda para la validacion del archivo CPM
+        $coordenadas = "A1";
+        $hojaActual = $documento->getSheet(0);
+        $celda = $hojaActual->getCell($coordenadas);
+        $valorRaw = $celda->getValue();
+
+
+        $listaCarreras = array("");
+        $listaMaterias = array();
+
+        // Sin comillas para objetos
+        $listaCarreraFinal = array();
+
+
+        $nombreDeCarrera = "";
+        $band = 0;
+        foreach ($hojaActual->getRowIterator() as $fila) {
+            foreach ($fila->getCellIterator() as $celdaPrueba) {
+
+                # El valor, así como está en el documento
+                $valorRaw = $celdaPrueba->getValue();
+
+
+                # Fila, que comienza en 1, luego 2 y así...
+                $fila = $celdaPrueba->getRow();
+                # Columna, que es la A, B, C y así...
+                $columna = $celdaPrueba->getColumn();
+
+
+                if ($valorRaw != "") {
+                    if ($columna == "B" || $columna == "D" || $columna == "C") {
+                        #Aqui metemos las materias en una lista
+                        #valorRaw nos da lo que tenemos en las celdas
+                        if ($columna == "B") {
+                            if ($valorRaw != "carrera") {
+                                array_push($listaCarreras, $valorRaw);
+                                if ($band == 0) {
+                                    $nombreDeCarrera = $valorRaw;
+                                    $band = 1;
+                                } else {
+                                    if ($nombreDeCarrera != $valorRaw) {
+
+                                        // CREACION DEL OBJETO
+                                        $carr = new Carrera($nombreDeCarrera, $listaMaterias,"");
+                                        // Inserta el objeto en el array de materiasPorCarrera
+                                        array_push($listaCarreraFinal, $carr);
+
+                                        while (count($listaMaterias)) array_pop($listaMaterias);
+                                        $band = 0;
+                                    }
+                                }
+                            }
+                        }
+                        if ($columna == "D") {
+
+
+                            if ($valorRaw != "materia") {
+                                $mat = new Materias($valorRaw,"","");
+                                array_push($listaMaterias, $mat);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return $listaCarreraFinal;
+    }
+
+    public function AsignaValor($listaCarreras,$listaMateriasGlobal)
+    {
+            $contador = 0;
+            foreach($listaCarreras as $lf)
+            {
+                //echo '<h2>'.$lf->nombreCarrera.'</h2>';
+                foreach($lf->listaMaterias as $listass){
+                    //ahi accedemos a cada materia de cada carrera
+                    foreach($listaMateriasGlobal as $mg)
+                    {
+                        //echo 'Se compara'.$listass->Materia.'Contra: '.$mg->nombreMateria;
+                       if($mg->nombreMateria == $listass->Materia)
+                       {
+                            $contador = $contador + 1;
+                            //echo '   entro'.'<br>';
+                       }else{
+                        //echo '   NO entro'.'<br>';
+                       }
+                    }
+                    $listass->valorGrupo = $contador;
+                    //echo 'La materia: '.$listass->Materia.'          es de valor: '.$contador;
+                    //echo '<br>';
+                    $contador = 0;
+                }
+            }
+
+
+
+
+            $contadorM = 0;
+            foreach($listaCarreras as $lf)
+            {
+                //echo '<h2>'.$lf->nombreCarrera.'</h2>';
+                foreach($lf->listaMaterias as $listass){
+                    //ahi accedemos a cada materia de cada carrera
+                    foreach($listaCarreras as $lf2)
+                    {
+                            foreach($lf2->listaMaterias as $mg)
+                        {
+                            //echo 'Se compara'.$listass->Materia.'Contra: '.$mg->Materia;
+                        if($mg->Materia == $listass->Materia)
+                        {
+                                $contadorM = $contadorM + 1;
+                            // echo '   entro'.'<br>';
+                        }else{
+                        // echo '   NO entro'.'<br>';
+                        }
+                    }
+                    }
+                    $listass->valorMateria = $contadorM;
+                    //echo 'La materia: '.$listass->Materia.'          es de valor Materia: '.$listass->valorMateria;
+                    //echo '<br>';
+                    $contadorM = 0;
+                }
+            }
+
+            $contadorMaterias = 0;
+            $acumulador = 0;
+            foreach($listaCarreras as $lf)
+            {
+               // echo '<h2>'.$lf->nombreCarrera.'</h2>';
+                foreach($lf->listaMaterias as $listass){
+
+                    $contador = $contador + 1;
+                    $acumulador = $acumulador + $listass->valorGrupo;
+                }
+                $lf->PromedioCarrera = ($acumulador/$contador);
+                //
+            }
+    }
 }
+
+
