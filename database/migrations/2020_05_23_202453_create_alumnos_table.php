2c61e3d7f7ec618a52d42a4268527fc3323a91ab
JonathanBaroL <jonathan.developer21@gmail.com>
2020-05-23 15:37:52 -0500

    alumnos1

---
+++ database/migrations/2020_05_23_202453_create_alumnos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlumnosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alumnos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('clave');
+            $table->string('alumno');
+            $table->string('calificacion');
+            $table->string('carrera');
+            $table->string('opcion');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alumnos');
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,6 +78,11 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('horarios') }}">{{ __('Horarios') }}</a>
                     </li>
+
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('alumnos') }}">{{ __('Alumnos') }}</a>
+                    </li>
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }} <span class="caret"></span>
