cf0217265914596c155b1b60425e9555fe1210af
JonathanBaroL <jonathan.developer21@gmail.com>
2020-08-17 23:26:29 -0500

    cambios casi finales de baro por aprobar

--- app/Horarios/Horarios.php
+++ app/Horarios/Horarios.php
@@ -19,58 +19,74 @@ if($res->num_rows > 0)
 
 
 
-foreach ($listaHorariosFinal as $lista) {
-  
-   if($lista->listaDia != null)//Si el arreglo no esta vacio
+foreach ($listaHorariosFinal2 as $final) {
+ 
+   $c2 = $final->numeroHorario;
+   foreach($final->listaDia as $horario)
    {
-    $c1 = $lista->listaDia[0]->carr;
-    $c2 = $lista->numeroHorario;
+      
+      $TotalCupo = 0;
+      foreach($horario as $p)
+      {
+         $c1 = $p->carr;
+         $c3 = $p->campo; // Quimica
+         $c4 = $p->hora; //08-09  
+         $c5 = $p->profesor;
+
+         $Lunes = "no";
+         $Martes = "no";
+         $Miercoles = "no";
+         $Jueves = "no";
+         $Viernes = "no";
+         $Sabado = "no";
+         
 
 
-    for ($i = 0; $i < count($lista->listaDia); $i++) {
+         $mystring = $p->dias;
 
-        $c3 = $lista->listaDia[$i]->campo;
-        $c4 = $lista->listaDia[$i]->hora;
-        $c5 = $lista->listaDia[$i]->profesor;
-        $Lunes = "no";
-        $Martes = "no";
-        $Miercoles = "no";
-        $Jueves = "no";
-        $Viernes = "no";
-        $Sabado = "no";
-        
-        $mystring = $lista->listaDia[$i]->dias;
 
-        $pos = strpos($mystring, "L");
-        if ($pos !== false) {
-           $Lunes = "si";
-        }
-        $pos = strpos($mystring, "Ma");
-        if ($pos !== false) {
-           $Martes = "si";
-        }
-        $pos = strpos($mystring, "Mi");
-        if ($pos !== false) {
-           $Miercoles = "si";
-        }
-        $pos = strpos($mystring, "J");
-        if ($pos !== false) {
-           $Jueves = "si";
-        }
-        $pos = strpos($mystring, "V");
-        if ($pos !== false) {
-           $Viernes = "si";
-        }
-        $pos = strpos($mystring, "S");
-        if ($pos !== false) {
-           $Sabado = "si";
-        }
+         $pos = strpos($mystring, "L");
+         if ($pos !== false) {
+            $Lunes = "si";
+         }
+         $pos = strpos($mystring, "Ma");
+         if ($pos !== false) {
+            $Martes = "si";
+         }
+         $pos = strpos($mystring, "Mi");
+         if ($pos !== false) {
+            $Miercoles = "si";
+         }
+         $pos = strpos($mystring, "J");
+         if ($pos !== false) {
+            $Jueves = "si";
+         }
+         $pos = strpos($mystring, "V");
+         if ($pos !== false) {
+            $Viernes = "si";
+         }
+         $pos = strpos($mystring, "S");
+         if ($pos !== false) {
+            $Sabado = "si";
+         }
 
         // Inserción 
         $insertar = "INSERT INTO horarios  VALUES (NULL, '$c1', '$c2', '$c3', '$c5', '$c4', '$Lunes', '$Martes', '$Miercoles', '$Jueves', '$Viernes', '$Sabado')";
 
         $query = mysqli_query($conexion, $insertar);
-    }
+
+         if($TotalCupo == 0)
+         {
+            $TotalCupo = $p->cupo;
+         }
+         else
+         {
+            if($TotalCupo > $p->cupo)
+            {
+               $TotalCupo = $p->cupo;
+            }
+         }
+      }
    }
 }
 
